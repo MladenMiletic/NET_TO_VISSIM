b286e64f6d48381b81751162de7070158efef6db
Mladen Miletić <mladenmileticlol@gmail.com>
2018-09-01 14:29:27 +0200

    minor change in licence info

--- NET_TO_VISSIM/UI/Meta.cs
+++ NET_TO_VISSIM/UI/Meta.cs
@@ -19,10 +19,10 @@ namespace NET_TO_VISSIM.UI
                 "you may not use this file except in compliance with the License.\n" +
                 "You may obtain a copy of the License at\n\n" +
                 "http://www.apache.org/licenses/LICENSE-2.0 \n\n" +
-                "Unless required by applicable law or agreed to in writing, software\n" +
-                "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-                "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-                "See the License for the specific language governing permissions and\n" +
+                "Unless required by applicable law or agreed to in writing, software " +
+                "distributed under the License is distributed on an \"AS IS\" BASIS, " +
+                "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. " +
+                "See the License for the specific language governing permissions and " +
                 "limitations under the License.", "Licence information!", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
         }
     }
