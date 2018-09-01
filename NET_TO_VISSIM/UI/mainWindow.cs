0450c7449edada47683a5363d2e4cece17962e0b
Mladen Miletić <mladenmileticlol@gmail.com>
2018-09-01 14:27:25 +0200

    Created separate UI folder, created static Meta class for UI, created licence info and forcet in on application start

---
+++ NET_TO_VISSIM/UI/mainWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NET_TO_VISSIM.UI;
+
+namespace NET_TO_VISSIM
+{
+    public partial class mainWindow : Form
+    {
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Load method for mainWindow
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mainWindow_Load(object sender, EventArgs e)
+        {
+            Meta.ShowLicenceInfo();
+        }
+    }
+}
