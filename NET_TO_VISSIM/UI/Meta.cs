948a45acfcb373c17ae99588c3fda83d2b3416e4
Mladen Miletić <mladenmileticlol@gmail.com>
2018-09-03 19:33:16 +0200

    Get for linceceInfo which is now a const string

--- NET_TO_VISSIM/UI/Meta.cs
+++ NET_TO_VISSIM/UI/Meta.cs
@@ -9,12 +9,7 @@ namespace NET_TO_VISSIM.UI
 {
     public static class Meta
     {
-        /// <summary>
-        /// Opens a message box showing licence information to the user
-        /// </summary>
-        public static void ShowLicenceInfo()
-        {
-            MessageBox.Show("Copyright 2018 Mladen Miletić\n\n" +
+        private const string licenceInfo = "Copyright 2018 Mladen Miletić\n\n" +
                 "Licensed under the Apache License, Version 2.0(the \"License\");\n" +
                 "you may not use this file except in compliance with the License.\n" +
                 "You may obtain a copy of the License at\n\n" +
@@ -23,7 +18,18 @@ namespace NET_TO_VISSIM.UI
                 "distributed under the License is distributed on an \"AS IS\" BASIS, " +
                 "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. " +
                 "See the License for the specific language governing permissions and " +
-                "limitations under the License.", "Licence information!", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                "limitations under the License.";
+        /// <summary>
+        /// Opens a message box showing licence information to the user
+        /// </summary>
+        public static void ShowLicenceInfo()
+        {
+            MessageBox.Show(licenceInfo, "Licence information!", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
         }
+        /// <summary>
+        /// Get method for licence info data
+        /// </summary>
+        /// <returns>String containing entire licence data</returns>
+        public static string GetLicenceInfo() => licenceInfo;
     }
 }
