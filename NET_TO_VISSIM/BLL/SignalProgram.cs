6e672747812d7cbe569fdd56a726f4d2939b003b
Mladen Miletić <mladenmileticlol@gmail.com>
2018-09-13 05:28:08 +0200

    Added signal program

--- NET_TO_VISSIM/BLL/SignalProgram.cs
+++ NET_TO_VISSIM/BLL/SignalProgram.cs
@@ -19,9 +19,19 @@ namespace NET_TO_VISSIM.BLL
         public int currentPhaseIndex;
         public Phase currentPhase;
 
+        /// <summary>
+        /// Constructor, if no offset is set it is considered to be 0
+        /// </summary>
+        /// <param name="phases">List of phases</param>
         public SignalProgram(List<Phase> phases) : this(phases, 0)
         {
         }
+
+        /// <summary>
+        /// Constructor, initialization
+        /// </summary>
+        /// <param name="phases">List of phases</param>
+        /// <param name="offset">Offset</param>
         public SignalProgram(List<Phase> phases, float offset)
         {
             this.phases = phases;
@@ -32,6 +42,9 @@ namespace NET_TO_VISSIM.BLL
             SetInitialPhase();
         }
 
+        /// <summary>
+        /// Sets initial phase depending on the offset
+        /// </summary>
         private void SetInitialPhase()
         {
             float checkedPhasesDuration = 0;
@@ -51,6 +64,9 @@ namespace NET_TO_VISSIM.BLL
             }
         }
 
+        /// <summary>
+        /// Calculates total cycle duration
+        /// </summary>
         private void CalculateDuration()
         {
             this.duration = 0;
@@ -60,6 +76,10 @@ namespace NET_TO_VISSIM.BLL
             }
         }
 
+        /// <summary>
+        /// Performs one step, activates proper phase
+        /// </summary>
+        /// <param name="resolution">Simulation resolution</param>
         public void Step(int resolution)
         {
             if (currentPhase.Step(resolution))
