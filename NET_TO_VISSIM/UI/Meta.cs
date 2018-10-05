2d08b4307c2c1a38096a4d8ddd7fa9d07aa0b67c
Mladen Miletic <mladenmileticlol@gmail.com>
2018-10-05 21:13:15 +0200

    licence meta data update

--- NET_TO_VISSIM/UI/Meta.cs
+++ NET_TO_VISSIM/UI/Meta.cs
@@ -13,15 +13,8 @@ namespace NET_TO_VISSIM.UI
     public static class Meta
     {
         private const string licenceInfo = "Copyright 2018 Mladen Miletić\n\n" +
-                "Licensed under the Apache License, Version 2.0(the \"License\");\n" +
-                "you may not use this file except in compliance with the License.\n" +
-                "You may obtain a copy of the License at\n\n" +
-                "http://www.apache.org/licenses/LICENSE-2.0 \n\n" +
-                "Unless required by applicable law or agreed to in writing, software " +
-                "distributed under the License is distributed on an \"AS IS\" BASIS, " +
-                "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. " +
-                "See the License for the specific language governing permissions and " +
-                "limitations under the License.";
+                "Licensed under the GNU LESSER GENERAL PUBLIC LICENSE";
+                
         /// <summary>
         /// Opens a message box showing licence information to the user
         /// </summary>
